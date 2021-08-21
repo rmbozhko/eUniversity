8efd432574ac0e099a6834b4fb68e1488da41a6d
rmbozhko <rm.bozhko@gmail.com>
2021-08-21 11:12:26 +0300

    Adding mandatory files

---
+++ src/main/java/edu/spring/euniversity/EUniversityApplication.java
@@ -0,0 +1,13 @@
+package edu.spring.euniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EUniversityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/spring/euniversity/EUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.spring.euniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EUniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
